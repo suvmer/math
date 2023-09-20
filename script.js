2f7e4327d297ddff991cb05abd1ef48d9fda9fce
duhan <duhan@LAPTOP-1BP174QR>
2023-09-20 19:48:40 +0300

    logic formula detection works correctly by 99%

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const getEl = (id) => document.querySelector(`#${id}`);
 const inputs = ["p1", "p2"];
-const outputs = ["out1", "out2"];
+const outputs = ["out1", "out2", "buttons", "symbols", "brackets"];
 const elems = Object.fromEntries(new Map([
     ["inputs", Object.fromEntries(new Map(inputs.map(id => [id, getEl(id)])))],
     ["outputs", Object.fromEntries(new Map(outputs.map(id => [id, getEl(id)])))]
@@ -12,24 +12,81 @@ elems.inputs.p1.addEventListener("input", (event) => {
     const nums = event.target.value.replace(/[^0-9\\-\\s]/g, "").split(" ").map(el => +el);
     console.log(nums);
 })
-let k = false;
+const alph = "abcdefghijklmnopqrstivwxyz";
+const brackets = ["(", ")"];
+const binary = ["⇔", "⇒", "⊕", "∨", "∧"];
+const unary = ["¬"];
+document.querySelector("#backspace").addEventListener("click", (event) => {
+    elems.inputs.p2.value = elems.inputs.p2.value.length > 0 ? elems.inputs.p2.value.slice(0, -1) : "";
+});
+
+const addToKeyboard = (nodeName, values, ...classNames) => {
+    values.forEach(el => {
+        const element = document.createElement('button');
+        element.innerHTML = el;
+        element.classList.add(...classNames);
+        element.addEventListener("click", (event) => {
+            elems.inputs.p2.focus();
+            let prev = elems.inputs.p2.selectionStart;
+            let text = event.target.innerHTML;
+            
+            const ev = new KeyboardEvent('keydown', { key: text });
+            elems.inputs.p2.dispatchEvent(ev);
+            let newValue = elems.inputs.p2.value.substring(0, prev) +
+            text + elems.inputs.p2.value.substring(prev);
+            elems.inputs.p2.value = newValue;// event.target.innerHTML;
+            elems.inputs.p2.selectionStart = prev + text.length;
+            elems.inputs.p2.selectionEnd = prev + text.length;
+            updateCheck(newValue)
+            //elems.inputs.p2.value += event.target.innerHTML;
+        })
+        elems.outputs[nodeName].appendChild(element);
+    })
+}
+addToKeyboard("buttons", [...brackets, ...binary, ...unary], "button")
+addToKeyboard("symbols", alph.split(""), "button", "button_yellow")
+document.body.addEventListener("click", (event) => elems.inputs.p2.focus());
+let anyCorrectChar = [...alph, ...brackets, ...binary, ...unary];
 let check = (str) => {
     let was = false;
     let stack = [];
     for(let symb of str) {
+        console.log(stack)
+        if(anyCorrectChar.indexOf(symb) === -1)
+            return false;
+        let last = stack[stack.length - 1];
         if(symb == '(') {
-            if(stack.length > 0 && ["(", "!"].find(stack[stack.length - 1]) === undefined)
+            if(stack.length > 0 && ["(", ...unary, ...binary].indexOf(last) === -1)
                 return false;
-            stack.push('(');
-        } else {
-            
+            stack.push(symb);
+        } else if(symb == ')'){
+            if(stack.length == 0 || [")", ...alph].indexOf(last) === -1)
+                return false;
+            while(stack.length > 0 && stack[stack.length - 1] != '(')
+                stack.pop();
+            stack.pop();
+            while(stack.length > 0 && [...unary, ...binary].indexOf(stack[stack.length - 1]) != -1)
+                stack.pop();
+        } else if(binary.indexOf(symb) !== -1) {
+            if(stack.length == 0 || [")", ...alph].indexOf(last) === -1)
+                return false;
+            stack.push(symb);
+        } else if(unary.indexOf(symb) !== -1) {
+            if(stack.length > 0 && ["(", ...alph, ...unary, ...binary].indexOf(last) === -1)
+                return false;
+            stack.push(symb);
+        } else {// if(alph.indexOf(symb) !== -1) {
+            if(stack.length == 0 || ["(", ...unary, ...binary].indexOf(last) === -1)
+                return false;
+            stack.push(symb);
         }
     }
-    return k;
+        console.log(stack)
+        return stack.length == 0 && str.length != 0;
+    //return k;
 }
-elems.inputs.p2.addEventListener("input", (event) => {
-    console.log()
-    if(check(event.target.value.trim())) {
+const updateCheck = (str) => {
+    if(check(str)) {
         elems.outputs.out2.classList.remove("red")
         elems.outputs.out2.classList.add("green")
         elems.outputs.out2.innerHTML = "Да, это формула"
@@ -38,4 +95,7 @@ elems.inputs.p2.addEventListener("input", (event) => {
         elems.outputs.out2.classList.add("red")
         elems.outputs.out2.innerHTML = "Нет, это не формула"
     }
+}
+elems.inputs.p2.addEventListener("input", (event) => {
+    updateCheck(event.target.value.trim());
 })
